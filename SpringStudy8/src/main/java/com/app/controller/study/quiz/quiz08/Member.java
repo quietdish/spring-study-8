aac71fa37eb2d5325b35fc95c9801b697f342612
quietdish <whale61@naver.com>
2026-08-06 14:10:19 +0900

    퀴즈8 jstl 활용 등 일단커밋만

---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz08/Member.java
@@ -0,0 +1,17 @@
+package com.app.controller.study.quiz.quiz08;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Member {
+
+	String id;
+	String pw;
+	String name;
+	String type;
+
+}
---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz08/Quiz08Controller.java
@@ -0,0 +1,8 @@
+package com.app.controller.study.quiz.quiz08;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Quiz08Controller {
+
+}
