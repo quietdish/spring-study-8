ec978d68ad961686894db4701be2a80822abf1d0
quietdish <whale61@naver.com>
2026-08-13 11:54:58 +0900

    quiz15 수정?

--- SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz15/Quiz15CustomerServiceImpl.java
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz15/Quiz15CustomerServiceImpl.java
@@ -4,18 +4,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class Quiz15CustomerServiceImpl implements Quiz15CustomerService{
+public class Quiz15CustomerServiceImpl implements Quiz15CustomerService {
 
 	@Autowired
 	Quiz15CustomerDAO quiz15CustomerDAO;
 	
 	@Override
-	public int saveCustomer(Quiz15Customer Quiz15Customer) {
+	public int saveCustomer(Quiz15Customer quiz15Customer) {
 		
-		System.out.println("[service] CustomerService");
-		//서비스 로직
+		System.out.println("[Service] CustomerService");
+		//서비스 로직  ~~
 		
+		// Service -> DAO   저장수행
 		int result = quiz15CustomerDAO.saveCustomer(quiz15Customer);
+
 		return result;
 	}
+
 }
