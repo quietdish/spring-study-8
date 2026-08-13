a9bb900dd8a67e319b51a0cb9e29eb68be5d909d
quietdish <whale61@naver.com>
2026-08-13 11:54:16 +0900

    DB연동 신규 Room 등록 데이터 Insert 저장, Mybatis-config 설정

--- SpringStudy8/src/main/java/com/app/controller/admin/AdminController.java
+++ SpringStudy8/src/main/java/com/app/controller/admin/AdminController.java
@@ -1,7 +1,10 @@
 package com.app.controller.admin;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +35,33 @@ public class AdminController {
 		int result = roomService.saveRoom(room);
 		
 		// result 값 확인 -> 성공/실패 -> 진행
+		System.out.println("insert 처리 결과 리턴받은 적용된 행의 수: " + result);
 		
-		return "/admin/registerRoom";
+		
+		if(result > 0) { //저장 성공
+			return "redirect:/admin/rooms";
+		} else {	//저장 실패
+			return "/admin/registerRoom";
+		}
+		
+				
+	}
+	
+	//관리자가 객실
+	@GetMapping("/admin/rooms")
+	public String rooms(Model model) {
+		
+		//rooms 페이지 
+		// T_ROOM 테이블 객실데이터	-> 조회	-> view 전달 -> 표시
+		
+		List<Room> roomList = roomService.findRoomList();
+		
+		model.addAttribute("roomList", roomList);
+		
+		return "admin/rooms";
 		
 		
 	}
+	
+	
 }
--- SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
+++ SpringStudy8/src/main/java/com/app/dao/room/impl/RoomDAOImpl.java
@@ -2,6 +2,8 @@ package com.app.dao.room.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.app.dao.room.RoomDAO;
@@ -15,16 +17,30 @@ import com.app.dto.room.Room;
 @Repository
 public class RoomDAOImpl implements RoomDAO {
 
+	//DB연동 처리할때 필요한 관련된 객체(Bean) 주입
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	
 	@Override
 	public List<Room> findRoomList() {
 
+		// DB 연동 -> room 테이블 데이터 조회 -> List<Room>		
 		System.out.println("RoomDAOImpl findRoomList");
-		return null;
+		
+		List<Room> roomList = sqlSessionTemplate.selectList("room_mapper.findRoomList");
+		
+		return roomList;
 	}
 
+	
 	@Override
 	public int saveRoom(Room room) {
 		// DB에 room 정보 테이블에 room 정보를 저장
-		return 0;
+								//실행할 쿼리 위치의 식별자, 매개변수
+		int result = sqlSessionTemplate.insert("room_mapper.saveRoom", room);
+		//수행 적용된 행의 수
+		
+		return result;
 	}
 }
