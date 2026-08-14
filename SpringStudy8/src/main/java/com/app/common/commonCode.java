a804282c704892e84f1650485833e45d0e0fa213
quietdish <whale61@naver.com>
2026-08-14 11:23:06 +0900

    공통코드 인터페이스 추가(코드 상수형 변수로 통합 정리)

---
+++ SpringStudy8/src/main/java/com/app/common/commonCode.java
@@ -0,0 +1,21 @@
+package com.app.common;
+
+public interface commonCode {
+
+	//프로젝트 내에 사용하는 모든 코드들을 모아두는 역할
+	//공통코드
+	
+	//USER
+	public static final String USER_USERTYPE_CUSTOMER = "CUS";
+	public static final String USER_USERTYPE_ADMIN = "ADM";
+	
+	//ROOM
+	public String ROOM_VIEWTYPE_OCEAN = "OCN";
+	public String ROOM_VIEWTYPE_CITY = "CTY";
+	public String ROOM_VIEWTYPE_MOUNTAIN = "MOT";
+	
+	//대상 코드
+	
+	
+	
+}
--- SpringStudy8/src/main/java/com/app/service/user/impl/UserServiceImpl.java
+++ SpringStudy8/src/main/java/com/app/service/user/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.app.common.commonCode;
 import com.app.dao.user.UserDAO;
 import com.app.dto.user.User;
 import com.app.service.user.UserService;
@@ -28,7 +29,8 @@ public class UserServiceImpl implements UserService {
 		
 		//사용자 계정 추가시 사용 메소드
 		//고객계정으로 추가!
-		user.setUserType("CUS");
+		//user.setUserType("CUS");
+		user.setUserType(commonCode.USER_USERTYPE_CUSTOMER );
 		
 		//int result = userDAO.saveUser(user);
 		return userDAO.saveUser(user);
@@ -38,7 +40,8 @@ public class UserServiceImpl implements UserService {
 	public int saveAdminUser(User user) {
 		//관리자 계정 추가시 사용 메소드
 		//관리자계정으로 추가!
-		user.setUserType("ADM");
+		//user.setUserType("ADM");
+		user.setUserType( commonCode.USER_USERTYPE_ADMIN );
 		
 		int result = userDAO.saveUser(user);
 		return result;
