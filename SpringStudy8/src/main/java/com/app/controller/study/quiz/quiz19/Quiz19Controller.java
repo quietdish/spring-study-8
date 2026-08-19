6c423b2787467659e28023a254bca2888fadc805
quietdish <whale61@naver.com>
2026-08-19 12:25:19 +0900

    퀴즈 19 REST API 응답처리

---
+++ SpringStudy8/src/main/java/com/app/controller/study/quiz/quiz19/Quiz19Controller.java
@@ -0,0 +1,39 @@
+package com.app.controller.study.quiz.quiz19;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.app.common.commonCode;
+import com.app.dto.user.User;
+
+
+@Controller
+//@RequestMapping("/quiz19")
+public class Quiz19Controller {
+	
+	@GetMapping("/quiz19/quiz1")
+	public String quiz1() {
+		return "quiz/quiz19/quiz1";
+	}
+	
+	
+	@ResponseBody
+	@GetMapping("/quiz19/quiz2")
+	public String quiz2() {
+		return "return text quiz2";
+	}
+
+	
+	@ResponseBody
+    @GetMapping("/quiz19/quiz3")
+    public User quiz3() {
+        User user = new User();
+        user.setId("quizid");
+        user.setPw("secret");
+        user.setName("퀴즈이름");
+        user.setUserType(commonCode.ROOM_VIEWTYPE_CITY);
+
+        return user;
+    }
+}
